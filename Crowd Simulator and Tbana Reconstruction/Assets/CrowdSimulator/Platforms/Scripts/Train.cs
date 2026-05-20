4e85d2d5f1c8555d9041ab1c5d7252c524c03fa0
jenna <smulter@kth.se>
2026-05-20 17:10:00 +0200

    Made train spawner burst rate private

--- Crowd Simulator and Tbana Reconstruction/Assets/CrowdSimulator/Platforms/Scripts/Train.cs
+++ Crowd Simulator and Tbana Reconstruction/Assets/CrowdSimulator/Platforms/Scripts/Train.cs
@@ -7,7 +7,7 @@ public class Train : MonoBehaviour
     public int numberOfAgents;
     internal List<TrainSpawner> trainSpawners;
     public List<CustomNode> goalNodes;
-    public float burstRate = 0.3f; // Time between agent spawns
+    private float burstRate = 0.3f; // Time between agent spawns
     public GameObject agentContainer;
     public Agent agentPrefab;
     public Material alightingAgentMaterial;
